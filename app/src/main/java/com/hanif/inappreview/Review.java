21f304654719060b43d7ff86da0c7c82e8710e5b
ha0986 <ha0986@outlook.com>
2022-11-30 06:44:07 +0600

    handling error

--- app/src/main/java/com/hanif/inappreview/Review.java
+++ app/src/main/java/com/hanif/inappreview/Review.java
@@ -1,13 +1,14 @@
 package com.hanif.inappreview;
 
 import android.app.Activity;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+
 import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
@@ -19,6 +20,7 @@ import java.util.Set;
 
 public class Review extends GodotPlugin {
     public Activity activity;
+
     public Review(Godot godot) {
         super(godot);
     }
@@ -37,11 +39,8 @@ public class Review extends GodotPlugin {
     }
 
 
-
-
-
     @UsedByGodot
-    public void review(){
+    public void review() {
         ReviewManager manager = ReviewManagerFactory.create(getActivity());
         Task<ReviewInfo> request = manager.requestReviewFlow();
         request.addOnCompleteListener(task -> {
@@ -49,10 +48,17 @@ public class Review extends GodotPlugin {
 
                 ReviewInfo reviewInfo = task.getResult();
                 Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
-                flow.addOnCompleteListener(okay-> {
+                flow.addOnCompleteListener(okay -> {
                 });
+            } else{
+                Log.d("error", "this app may be already rated or this app isn't in play store");
             }
+
         });
     }
+
+
+
+
 
 }
